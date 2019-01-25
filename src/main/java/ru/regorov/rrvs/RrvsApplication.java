f85ac3e89be88318536677c3538b81838267b1b9
tral909 <indorm1992@mail.ru>
2019-01-26 00:22:50 +0300

    init project

---
+++ src/main/java/ru/regorov/rrvs/RrvsApplication.java
@@ -0,0 +1,14 @@
+package ru.regorov.rrvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrvsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RrvsApplication.class, args);
+    }
+
+}
+
