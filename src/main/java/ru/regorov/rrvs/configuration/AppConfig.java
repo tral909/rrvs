40a142c5e97ffa30fa89425e4258ef2e3780eae6
tral909 <indorm1992@mail.ru>
2019-01-29 07:40:05 +0300

    configure serializing from props and add comment for db server starting

--- src/main/java/ru/regorov/rrvs/configuration/AppConfig.java
+++ src/main/java/ru/regorov/rrvs/configuration/AppConfig.java
@@ -1,6 +1,5 @@
 package ru.regorov.rrvs.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -9,7 +8,6 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import ru.regorov.rrvs.controller.json.JacksonObjectMapper;
 
 @Configuration
 public class AppConfig {
@@ -18,6 +16,13 @@ public class AppConfig {
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.hsqldb.jdbc.JDBCDriver");
+        //////start standaone server for multiple connections to database
+        //cd C:\Users\Roman\.m2\repository\org\hsqldb\hsqldb\2.4.1
+        //mkdir db; cd db
+        //java -cp .\hsqldb-2.4.1.jar org.hsqldb.Server -database.0 mem:rrvs
+        //java -cp ..\hsqldb-2.4.1.jar org.hsqldb.Server --database.0 file:mydb --dbname.0 rrvs
+        //dataSource.setUrl("jdbc:hsqldb:hsql://localhost:9001/rrvs");
+        //////start database in memory or in file
         dataSource.setUrl("jdbc:hsqldb:mem:rrvs");
         //dataSource.setUrl("jdbc:hsqldb:file:D:/temp/rrsv");
         dataSource.setUsername("sa");
@@ -32,8 +37,8 @@ public class AppConfig {
         return dataSource;
     }
 
-    @Bean
+    /*@Bean
     public ObjectMapper objectMapper() {
         return JacksonObjectMapper.getMapper();
-    }
+    }*/
 }
