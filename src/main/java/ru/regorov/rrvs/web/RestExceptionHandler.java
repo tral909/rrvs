8cd0ca1eb9db456d2caf6a66389d1f4afb5ecd98
tral909 <indorm1992@mail.ru>
2019-03-10 00:18:50 +0300

    finish exception handlers

--- src/main/java/ru/regorov/rrvs/web/RestExceptionHandler.java
+++ src/main/java/ru/regorov/rrvs/web/RestExceptionHandler.java
@@ -4,44 +4,64 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import ru.regorov.rrvs.util.ValidationUtil;
-import ru.regorov.rrvs.util.exceptions.EndVoteException;
-import ru.regorov.rrvs.util.exceptions.ErrorType;
-import ru.regorov.rrvs.util.exceptions.NotFoundException;
-import ru.regorov.rrvs.util.exceptions.RestError;
+import ru.regorov.rrvs.util.exceptions.*;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
     private static Logger log = LoggerFactory.getLogger(RestExceptionHandler.class);
+    private static final String USER_UNIQUE_LOGIN = "user_unique_login_idx";
+    private static final String USER_ROLE_UNIQUE = "user_roles_idx";
+    private static final String RESTAURANT_UNIQUE_PHONE = "restaurant_unique_phone_idx";
+    private static final String MENU_DISH_UNIQUE = "menu_dish_idx";
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
     public RestError handleNotFound(NotFoundException e) {
-        return new RestError(ErrorType.DATA_NOT_FOUND_ERROR, ValidationUtil.getMessage(e));
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        return new RestError(ErrorType.DATA_NOT_FOUND_ERROR, ValidationUtil.getMessage(rootCause));
     }
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(EndVoteException.class)
     public RestError handleEndVoting(EndVoteException e) {
-        return new RestError(ErrorType.END_VOTE_ERROR, ValidationUtil.getMessage(e));
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        return new RestError(ErrorType.END_VOTE_ERROR, ValidationUtil.getMessage(rootCause));
     }
 
-    //TODO доделать здесь все unique ограничения из базы
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(DataIntegrityViolationException.class)
     public RestError handleDataIntergityViolation(DataIntegrityViolationException e) {
         Throwable rootCause = ValidationUtil.getRootCause(e);
         String message = ValidationUtil.getMessage(rootCause).toLowerCase();
-        if (message.contains("user_unique_login_idx")) {
+        if (message.contains(USER_UNIQUE_LOGIN)) {
             message = "Пользователь с таким полем 'login' уже существует";
-        } else if (message.contains("user_roles_idx")) {
+        } else if (message.contains(USER_ROLE_UNIQUE)) {
             message = "Пользователь с такой ролью уже существует";
+        } else if (message.contains(RESTAURANT_UNIQUE_PHONE)) {
+            message = "Ресторан с таким полем 'phone' уже существует";
+        } else if (message.contains(MENU_DISH_UNIQUE)) {
+            message = "В этом меню такое блюдо уже добавлено";
         }
         return new RestError(ErrorType.DATA_ERROR, message);
     }
 
-    //TODO закончить с остальными исключениями
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler({IllegalRequestDataException.class, MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class})
+    public RestError illegalRequestDataError(Exception e) {
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        return new RestError(ErrorType.VALIDATION_ERROR, ValidationUtil.getMessage(rootCause));
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public RestError handleError(Exception e) {
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        return new RestError(ErrorType.APP_ERROR, ValidationUtil.getMessage(rootCause));
+    }
 }
